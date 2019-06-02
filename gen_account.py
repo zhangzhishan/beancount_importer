95fa4bb8be597a5028d07a499a74c8d13204e97c
Zhishan Zhang <zhangzhishanlo@gmail.com>
2019-06-02 17:22:26 +0800

    gen padding and balance for accounts

--- gen_account.py
+++ gen_account.py
@@ -4,6 +4,7 @@ import os
 import io
 import sys
 from os import path
+import datetime
 
 def load_json(filename):
     fd = open(filename, 'r', encoding="UTF-8")
@@ -20,6 +21,20 @@ if __name__ == '__main__':
         if key == "expenses":
             for account in mapping.values():
                 print("1970-01-01 open Expenses:" + account)
+        elif key == "accounts":
+            for account in mapping.values():
+                print("1970-01-01 open Expenses:" + account)
+            # add balance and pad for accounts
+            time = datetime.datetime.now().strftime('%Y-%m-%d')
+
+            print(";-------------------------START INITIALIZE ACCOUNTS-------------------")
+            print(";-------------------------PADDING ACCOUNTS----------------------------")
+            for account in mapping.values():
+                print("1970-01-01 pad " + account + " Equity:Opening-Balances")
+            print(";-------------------------BALANCE ACCOUNTS----------------------------")
+            for account in mapping.values():
+                print(time + " balance " + account + " 0.00 CNY")
+            print(";-------------------------END INITIALIZE ACCOUNTS-------------------")
         else:
             if key != "tags":
                 for account in mapping.values():
